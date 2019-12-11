493ecd0889fb70ef58464cda6890f3ddeff67206
nk <nktuhin12@gmail.com>
2019-12-11 16:28:45 +0600

    Nuxtjs Navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <v-app-bar>
+      <v-toolbar-title>
+        <n-link class="white--text" to="/">Weather App</n-link>
+      </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+      <v-btn to="/about" rounded color="primary" dark>About</v-btn>
+    </v-app-bar>
+  </div>
+</template>
+
+<style>
+a {
+  text-decoration: none;
+  color: white;
+}
+</style>
