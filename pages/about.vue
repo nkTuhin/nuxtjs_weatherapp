b7dff9bef7cf2288d839c1976820378636c0686b
nk <nktuhin12@gmail.com>
2019-12-11 16:30:04 +0600

    About page

---
+++ pages/about.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <v-container grid-list-xs>
+      <div>
+        <div>
+          <Navbar />
+        </div>
+        <h1>Simple weather app created by using Nuxtjs Framework for Vuejs</h1>
+      </div>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar'
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
